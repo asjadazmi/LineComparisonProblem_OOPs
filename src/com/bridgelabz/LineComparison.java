33900b4b436091062c83e77fa78b5dd764d076e9
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 21:49:55 +0530

    Added : Checkin the line Equality

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,27 +1,43 @@
 package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparison {
-	static int x1,x2,y1,y2;
-	static double length_of_the_line;
-	
-	public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("enter the value of x1");
-	x1=sc.nextInt();
-    System.out.println("enter the value of x2");
-	x2=sc.nextInt();
-	System.out.println("enter the value of y1");
-	y1=sc.nextInt();
-	System.out.println("enter the value of y2");
-	y2=sc.nextInt();
-	
-	showlinelength();
-	}
-	static void showlinelength( ) {
-		 length_of_the_line=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		System.out.println(length_of_the_line);
-	}
-	
+	   public static void main(String[] args) {
+           int x1, x2, x3, x4, y1, y2, y3, y4;
+           Scanner sc = new Scanner(System.in);
+           System.out.println("Enter the value x1 ");
+           x1 = sc.nextInt();
+           System.out.println("enter the value of x2");
+           y1 = sc.nextInt();
+           System.out.println("enter the value of y1");
+           x2 = sc.nextInt();
+           System.out.println("enter the value of y2");
+           y2 = sc.nextInt();
 
-	}
-	
+           System.out.println("Enter the value x3");
+           x3 = sc.nextInt();
+           System.out.println("enter the value of y3");
+           y3 = sc.nextInt();
+           System.out.println("enter the value of x4");
+           x4 = sc.nextInt();
+           System.out.println("enter the value of y4");
+           y4 = sc.nextInt();
+
+           Equality(x1, x2, x3, x4, y1, y2, y3, y4);
+   }
+	static double Equality (int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4) {
+        double a, b;
+
+        a = (double) Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        b = (double) Math.sqrt((x4 - x3)*(x4-x3) + (y4 - y3)*(y4-y3));
+
+        if (a == b) {
+            System.out.println("Both lines are equal.");
+        }
+        else {
+            System.out.println("Both lines are not equal.");
+        }
+        return 0;
+    }
+
+     
+}
